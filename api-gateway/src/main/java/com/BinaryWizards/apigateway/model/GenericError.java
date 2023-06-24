36da59a7566fbe4e8087ef713ccf0daa88d9855a
BarhaPritha <barha.pritha@gmail.com>
2023-06-24 22:46:04 +0600

    Add files

---
+++ api-gateway/src/main/java/com/BinaryWizards/apigateway/model/GenericError.java
@@ -0,0 +1,16 @@
+package com.BinaryWizards.apigateway.model;
+
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenericError {
+
+    private String message;
+
+}
---
+++ authentication-service/src/main/java/com/BinaryWizards/authenticationserver/exception/InvalidRoleException.java
@@ -0,0 +1,9 @@
+package com.BinaryWizards.authenticationserver.exception;
+
+public class InvalidRoleException extends RuntimeException {
+
+    public InvalidRoleException(String roleName) {
+        super("Invalid Role ( " + roleName + " )");
+    }
+
+}
